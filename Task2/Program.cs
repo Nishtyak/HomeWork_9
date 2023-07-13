163cc2b73428a04a5e3345b96c0d17ce55de322c
Tatyana <KrukovaTatyana1990@gmail.com>
2023-07-13 14:05:08 +0300

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int m = EnterNumberInt("Введите меньшее число: ");
+int n = EnterNumberInt("Введите большее число: ");
+Console.WriteLine($"M = {m}; N = {n} -> {Sum(m, n)}");
+
+int EnterNumberInt(string message)
+{
+    int num = 0;
+    Console.Write(message);
+    while(!int.TryParse(Console.ReadLine(), out num))
+        Console.WriteLine("Введите число!");
+    return num;
+}
+
+int Sum(int m, int n)
+{
+    if(m == n)
+    {
+        return m;
+    }
+    return Sum(m + 1, n) + m;
+}
